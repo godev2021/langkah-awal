b569c0744ae1d680cc00e36cf6db605969f812d2
hendralw <hendrawan@sg-dsa.com>
2025-07-26 19:31:57 +0700

    AI summarize feedback

--- src/main/java/com/langkah_awal/demo/model/request/PromptRequest.java
+++ src/main/java/com/langkah_awal/demo/model/request/PromptRequest.java
@@ -2,7 +2,15 @@ package com.langkah_awal.demo.model.request;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PromptRequest {
     String prompt;
+
+    @Data
+    public static class Employee {
+        String employeeName;
+        List<String> feedbacks;
+    }
 }
