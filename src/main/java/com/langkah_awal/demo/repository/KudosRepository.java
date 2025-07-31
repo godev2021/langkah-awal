765643472928a56ac4d52ebe21180141953a579a
fajarrifai <fajar.rifai@sg-dsa.com>
2025-07-31 20:34:04 +0700

    feat: add feature leaderboard kudos, employee score and update data

--- src/main/java/com/langkah_awal/demo/repository/KudosRepository.java
+++ src/main/java/com/langkah_awal/demo/repository/KudosRepository.java
@@ -22,21 +22,33 @@ public interface KudosRepository extends JpaRepository<Kudos, Long> {
     List<Kudos> findKudosByToEmployeeId(long employeeId);
 
     @Query(
-            value = "SELECT COUNT(DISTINCT from_employee_id) FROM kudos WHERE to_employee_id = :toEmployeeId AND EXTRACT(YEAR FROM timestamp) = EXTRACT(YEAR FROM CURRENT_DATE)",
-            nativeQuery = true)
-    int countDistinctKudosGiversThisYear(@Param("toEmployeeId") long toEmployeeId);
-
-    @Query(
-            value = "SELECT " +
-                    "e.name AS employeeName, " +
-                    "k.category AS category, " +
-                    "COUNT(DISTINCT k.from_employee_id) AS totalKudos, " +
-                    "SUM(k.score) AS totalScore " +
-                    "FROM kudos k " +
-                    "JOIN employee e ON k.to_employee_id = e.id " +
-                    "WHERE EXTRACT(YEAR FROM k.timestamp) = EXTRACT(YEAR FROM CURRENT_DATE) " +
-                    "GROUP BY k.to_employee_id, e.name " +
-                    "ORDER BY totalScore DESC",
+            value = """
+                    SELECT 
+                        e.name AS employeeName,
+                        k.category AS category,
+                        summary.totalKudos,
+                        summary.totalScore
+                    FROM kudos k
+                    JOIN (
+                        SELECT 
+                            k1.to_employee_id,
+                            MAX(k1.timestamp) AS latestTimestamp
+                        FROM kudos k1
+                        WHERE EXTRACT(YEAR FROM k1.timestamp) = EXTRACT(YEAR FROM CURRENT_DATE)
+                        GROUP BY k1.to_employee_id
+                    ) latest ON k.to_employee_id = latest.to_employee_id AND k.timestamp = latest.latestTimestamp
+                    JOIN (
+                        SELECT 
+                            k2.to_employee_id,
+                            COUNT(DISTINCT k2.from_employee_id) AS totalKudos,
+                            SUM(k2.score) AS totalScore
+                        FROM kudos k2
+                        WHERE EXTRACT(YEAR FROM k2.timestamp) = EXTRACT(YEAR FROM CURRENT_DATE)
+                        GROUP BY k2.to_employee_id
+                    ) summary ON k.to_employee_id = summary.to_employee_id
+                    JOIN employee e ON k.to_employee_id = e.id
+                    ORDER BY summary.totalScore DESC
+                    """,
             nativeQuery = true)
     List<KudosLeaderboardProjections> findKudosLeaderboardThisYear();
 }
